b31a6f562c4beff1fcd981584d6d0467116c2fad
Amber Cronin <amber@amber.vision>
2022-08-02 08:53:42 +0100

    Add more sane defaults for Darwin

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -5,4 +5,5 @@
 alias nano='nano --rcfile $HOME/.dotfiles/.nanorc.darwin'
 
 export EDITOR=/usr/local/bin/nano
-export PATH="/usr/local/bin:$PATH"
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
+export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/Brewfile"
