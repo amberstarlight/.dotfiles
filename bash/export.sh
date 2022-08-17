2ce6bba7ec167a46a5e6b4892bc79a23d0bd23ca
Amber Cronin <amber@amber.vision>
2022-08-17 09:39:39 +0100

    Add dotfiles/bin to PATH - Remove .sh extension from sync-dotfiles so I can use it without   specifying extension, you know, like a real binary :3

--- bash/export.sh
+++ bash/export.sh
@@ -11,3 +11,6 @@ export HISTIGNORE="&:??:[ ]*:clear:exit:logout"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Add ../bin so we can use sync-dotfiles anywhere
+export PATH="$PATH:$HOME/.dotfiles/bin"
