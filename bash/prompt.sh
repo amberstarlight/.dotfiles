6318d0c5b89c4bb1b4e6314ad730f20720ef2feb
Amber Cronin <amber@amber.vision>
2022-08-24 15:50:41 +0100

    Refactor prompt - Makes the prompt more readable, because nobody can actually read   escape sequences... Unless?

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -2,9 +2,43 @@
 # prompt.sh
 #
 
-promptTime="\n\[$(tput sgr0)\]\[\033[38;5;8m\]\D{%Y-%m-%d} \@\[$(tput sgr0)\]"
-promptDir="\n\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;9m\]\w\[$(tput sgr0)\]"
-promptGit="\n\[$(tput sgr0)\]\[\033[38;5;6m\]\[\$(git symbolic-ref --short HEAD 2>/dev/null)\]"
-promptHostDirGit="\n\[$(tput sgr0)\]\[\033[38;5;2m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;9m\]\w\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')\[$(tput sgr0)\]"
-promptEnd="\n\[$(tput sgr0)\]\[\033[38;5;8m\]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
-PS1=$promptTime$promptHostDirGit$promptEnd
+# Standard colours
+ST_BLK="\[$(tput setaf 0)\]"
+ST_RED="\[$(tput setaf 1)\]"
+ST_GRN="\[$(tput setaf 2)\]"
+ST_YLW="\[$(tput setaf 3)\]"
+ST_BLU="\[$(tput setaf 4)\]"
+ST_PRP="\[$(tput setaf 5)\]"
+ST_CYN="\[$(tput setaf 6)\]"
+ST_GRY="\[$(tput setaf 7)\]"
+
+# High-intensity colours
+HI_GRY="\[$(tput setaf 8)\]"
+HI_RED="\[$(tput setaf 9)\]"
+HI_GRN="\[$(tput setaf 10)\]"
+HI_YLW="\[$(tput setaf 11)\]"
+HI_BLU="\[$(tput setaf 12)\]"
+HI_PRP="\[$(tput setaf 13)\]"
+HI_CYN="\[$(tput setaf 14)\]"
+HI_WHT="\[$(tput setaf 15)\]"
+
+# tput capabilities
+BLD="\[$(tput bold)\]"
+DIM="\[$(tput dim)\]"
+RST="\[$(tput sgr0)\]"
+
+# bash special characters
+HOST="\h"
+USER="\u"
+WDIR="\w"
+
+# Determine git branch
+gitBr="\[\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')\]"
+
+promptTime="${HI_GRY}\D{%Y-%m-%d} \@${RST}"
+promptHost="${ST_GRN}${USER}${RST}${HI_GRY}@${RST}${ST_GRN}${HOST}${RST}"
+promptDir="${BLD}${HI_RED}${WDIR}${RST}"
+promptGit="${HI_CYN}${gitBr}${RST}"
+promptEnd="\n${HI_GRY}\$ ${RST}"
+
+PS1="\n${promptTime}\n${promptHost} ${promptDir} ${promptGit}${promptEnd}"
