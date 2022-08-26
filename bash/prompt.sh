f85e6b2a6951906b46b891c883e55ab71d51319c
Amber Cronin <amber@amber.vision>
2022-08-26 20:28:10 +0100

    Apply fixes from shellcheck output

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,3 +1,6 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2034
+
 #
 # prompt.sh
 #
--- bash/sierra.sh
+++ bash/sierra.sh
@@ -1,8 +1,10 @@
+# shellcheck shell=bash
+
 #
 # sierra.sh
 #
 
-if [ $(uname -n) == "sierra" ]; then # we are on the home machine
+if [ "$(uname -n)" == "sierra" ]; then # we are on the home machine
   export GIT_REPO_DIR=/storage/git
 
   # include Borg Backup secrets
@@ -11,7 +13,7 @@ if [ $(uname -n) == "sierra" ]; then # we are on the home machine
   fi
 
   backup () {
-    $GIT_REPO_DIR/backup/backup.sh |& tee -a "$HOME"/.log/"borg-$(echo "$HOSTNAME")-$(date +"%Y-%m-%d").log"
+    $GIT_REPO_DIR/backup/backup.sh |& tee -a "$HOME/.log/borg-sierra-$(date +"%Y-%m-%d").log"
     if [ "$1" = "shutdown" ]; then
       sudo shutdown
     fi
