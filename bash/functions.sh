3bdde5c0c57f9efacacdb6816f7a3ec5a16b3dc4
Amber Cronin <amber@amber.vision>
2022-09-15 20:07:23 +0100

    Improve dot_clean - Use a temporary file to store results - Echo results total after deleting

--- bash/functions.sh
+++ bash/functions.sh
@@ -6,13 +6,17 @@
 
 dot_clean () {
   local dir=${1:-.};
+  local tmp_file=".tmp-$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 10 | head -n 1)";
 
-  find "$dir" -type f \( 	\
-    -iname "._*" -o		\
-    -iname ".DS_Store" -o	\
-    -iname ".Spotlight-V100" -o \
-    -iname ".Trashes" -o	\
-    -iname "Thumbs.db" -o	\
-    -iname ".apdisk"		\
-  \) -delete;
+  find "$dir" -maxdepth 1 -type f \(     \
+    -iname "._*" -o                      \
+    -iname ".DS_Store" -o                \
+    -iname ".Spotlight-V100" -o          \
+    -iname ".Trashes" -o                 \
+    -iname "Thumbs.db" -o                \
+    -iname ".apdisk"                     \
+  \) -delete -fprintf "$tmp_file" "$dir";
+
+  echo "Processed $(wc -c <"$tmp_file") items.";
+  \rm "$tmp_file";
 }
