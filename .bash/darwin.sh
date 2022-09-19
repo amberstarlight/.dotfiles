27fa097e74190e4be9e60a6435885a2362a22d5f
Amber Cronin <amber@amber.vision>
2022-09-19 19:19:58 +0100

    Rename Brewfile to .brewfile, hiding it from view

--- .bash/darwin.sh
+++ .bash/darwin.sh
@@ -5,6 +5,6 @@
 #
 
 export EDITOR=/usr/local/bin/nano
-export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/Brewfile"
+export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/.brewfile"
 export NANO_RC_FILE="${HOME}/.dotfiles/.nanorc.darwin"
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
