1d97c9c9357fbc4ceca016cd3949bdb727ccf061
Amber Cronin <amber@amber.vision>
2022-09-19 12:40:13 +0100

    Add $HOME/.local/bin to PATH for pipenv

--- bash/sierra.sh
+++ bash/sierra.sh
@@ -4,18 +4,14 @@
 # sierra.sh
 #
 
-if [ "$(uname -n)" == "sierra" ]; then # we are on the home machine
-  export GIT_REPO_DIR=/storage/git
+export GIT_REPO_DIR=/storage/git
+export PATH="$PATH:$HOME/.local/bin"
 
-  # include Borg Backup secrets
-  if [ -f "$HOME"/.borg_secrets  ]; then
-    source "$HOME"/.borg_secrets
-  fi
-
-  backup () {
-    $GIT_REPO_DIR/backup/backup.sh |& tee -a "$HOME/.log/borg-sierra-$(date +"%Y-%m-%d").log"
-    if [ "$1" = "shutdown" ]; then
-      sudo shutdown
-    fi
-  }
+# include Borg Backup secrets
+if [ -f "$HOME"/.borg_secrets  ]; then
+  source "$HOME"/.borg_secrets
 fi
+
+backup () {
+  $GIT_REPO_DIR/backup/backup.sh |& tee -a "$HOME/.log/borg-sierra-$(date +"%Y-%m-%d").log"
+}
