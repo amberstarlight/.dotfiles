f4e502fee5026eddf03e95201b084b847ca80b65
Amber Cronin <amber@amber.vision>
2022-12-24 20:10:52 +0000

    Add /usr/local/sbin to Darwin, on the advice of brew

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -7,4 +7,4 @@
 export EDITOR=/usr/local/bin/nano
 export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/.brewfile"
 export NANO_RC_FILE="${HOME}/.dotfiles/.nanorc.darwin"
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:$PATH"
