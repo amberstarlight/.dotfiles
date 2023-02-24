df2a3ab77fed2206d6f6d0eb91fe0f6dd3a509f4
Amber Cronin <amber@amber.vision>
2023-02-24 10:22:20 +0000

    Add Ruby path for Darwin

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -7,4 +7,5 @@
 export EDITOR=/usr/local/bin/nano
 export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/.brewfile"
 export NANO_RC_FILE="${HOME}/.dotfiles/.nanorc.darwin"
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/ruby/bin:$PATH"
+# todo: make PATH up from an array/list
