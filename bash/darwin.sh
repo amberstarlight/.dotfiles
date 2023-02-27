f9b42464cccc3ac41a17c08c6e300ac354c23658
Amber Cronin <amber@amber.vision>
2023-02-27 10:20:06 +0000

    Add ruby, rbenv to brewfile

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -9,3 +9,4 @@ export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/.brewfile"
 export NANO_RC_FILE="${HOME}/.dotfiles/.nanorc.darwin"
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/ruby/bin:$PATH"
 # todo: make PATH up from an array/list
+eval "$(rbenv init - bash)"
