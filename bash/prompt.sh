b3ab64bf3dd6c6cec02e1ebd5e6cadad480e109d
Amber Cronin <amber@amber.vision>
2023-04-02 15:10:49 +0100

    Add PROMPT_COMMAND, move to using function

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -6,34 +6,29 @@
 #
 
 # Standard colours
-ST_BLK="\[$(tput setaf 0)\]"
-ST_RED="\[$(tput setaf 1)\]"
-ST_GRN="\[$(tput setaf 2)\]"
-ST_YLW="\[$(tput setaf 3)\]"
-ST_BLU="\[$(tput setaf 4)\]"
-ST_PRP="\[$(tput setaf 5)\]"
-ST_CYN="\[$(tput setaf 6)\]"
-ST_GRY="\[$(tput setaf 7)\]"
+ST_BLK="$(tput setaf 0)"
+ST_RED="$(tput setaf 1)"
+ST_GRN="$(tput setaf 2)"
+ST_YLW="$(tput setaf 3)"
+ST_BLU="$(tput setaf 4)"
+ST_PRP="$(tput setaf 5)"
+ST_CYN="$(tput setaf 6)"
+ST_GRY="$(tput setaf 7)"
 
 # High-intensity colours
-HI_GRY="\[$(tput setaf 8)\]"
-HI_RED="\[$(tput setaf 9)\]"
-HI_GRN="\[$(tput setaf 10)\]"
-HI_YLW="\[$(tput setaf 11)\]"
-HI_BLU="\[$(tput setaf 12)\]"
-HI_PRP="\[$(tput setaf 13)\]"
-HI_CYN="\[$(tput setaf 14)\]"
-HI_WHT="\[$(tput setaf 15)\]"
+HI_GRY="$(tput setaf 8)"
+HI_RED="$(tput setaf 9)"
+HI_GRN="$(tput setaf 10)"
+HI_YLW="$(tput setaf 11)"
+HI_BLU="$(tput setaf 12)"
+HI_PRP="$(tput setaf 13)"
+HI_CYN="$(tput setaf 14)"
+HI_WHT="$(tput setaf 15)"
 
 # tput capabilities
-BLD="\[$(tput bold)\]"
-DIM="\[$(tput dim)\]"
-RST="\[$(tput sgr0)\]"
-
-# bash special characters
-hostname="\h"
-username="\u"
-workdir="\w"
+BLD="$(tput bold)"
+DIM="$(tput dim)"
+RST="$(tput sgr0)"
 
 hostColour() {
   if is_ssh; then
@@ -43,10 +38,29 @@ hostColour() {
   fi
 }
 
-promptTime="${HI_GRY}$(date +%c)${RST}"
-promptHost="${ST_GRN}${username}${RST}${HI_GRY}@${RST}$(hostColour)${hostname}${RST}"
-promptDir="${BLD}${ST_RED}${workdir}${RST}"
-promptGit="${ST_CYN}\$(git_current_branch)${RST}"
-promptEnd="\n${HI_GRY}> ${RST}"
+exitStatus() {
+  if [[ "$1" -ne 0 ]]; then
+    echo "${HI_RED}"
+  else
+    echo "${ST_GRN}"
+  fi
+}
 
-PS1="\n${promptTime}\n${promptHost} ${promptDir} ${promptGit} ${promptEnd}"
+generatePrompt() {
+  local exit_status="$?"
+  local workdir="\w"
+  local current_user="\u"
+  local hostname="\h"
+
+  # do this first so we don't override $?
+  promptEnd="\n$(exitStatus exit_status)> ${RST}"
+
+  promptTime="${HI_GRY}$(date +%c)${RST}"
+  promptHost="${ST_GRN}${current_user}${RST}${HI_GRY}@${RST}$(hostColour)${hostname}${RST}"
+  promptDir="${BLD}${ST_RED}${workdir}${RST}"
+  promptGit="${ST_CYN}$(git_current_branch)${RST}"
+
+  PS1="\n${promptTime}\n${promptHost} ${promptDir} ${promptGit} ${promptEnd}"
+}
+
+PROMPT_COMMAND=generatePrompt
