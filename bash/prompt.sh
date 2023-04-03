bd61cb161e0b489949e0ac706a394bcfbcbb365c
Amber Cronin <amber@amber.vision>
2023-04-03 21:24:36 +0100

    Re-add non-printing character delimiters

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -6,29 +6,29 @@
 #
 
 # Standard colours
-ST_BLK="$(tput setaf 0)"
-ST_RED="$(tput setaf 1)"
-ST_GRN="$(tput setaf 2)"
-ST_YLW="$(tput setaf 3)"
-ST_BLU="$(tput setaf 4)"
-ST_PRP="$(tput setaf 5)"
-ST_CYN="$(tput setaf 6)"
-ST_GRY="$(tput setaf 7)"
+ST_BLK="\[$(tput setaf 0)\]"
+ST_RED="\[$(tput setaf 1)\]"
+ST_GRN="\[$(tput setaf 2)\]"
+ST_YLW="\[$(tput setaf 3)\]"
+ST_BLU="\[$(tput setaf 4)\]"
+ST_PRP="\[$(tput setaf 5)\]"
+ST_CYN="\[$(tput setaf 6)\]"
+ST_GRY="\[$(tput setaf 7)\]"
 
 # High-intensity colours
-HI_GRY="$(tput setaf 8)"
-HI_RED="$(tput setaf 9)"
-HI_GRN="$(tput setaf 10)"
-HI_YLW="$(tput setaf 11)"
-HI_BLU="$(tput setaf 12)"
-HI_PRP="$(tput setaf 13)"
-HI_CYN="$(tput setaf 14)"
-HI_WHT="$(tput setaf 15)"
+HI_GRY="\[$(tput setaf 8)\]"
+HI_RED="\[$(tput setaf 9)\]"
+HI_GRN="\[$(tput setaf 10)\]"
+HI_YLW="\[$(tput setaf 11)\]"
+HI_BLU="\[$(tput setaf 12)\]"
+HI_PRP="\[$(tput setaf 13)\]"
+HI_CYN="\[$(tput setaf 14)\]"
+HI_WHT="\[$(tput setaf 15)\]"
 
 # tput capabilities
-BLD="$(tput bold)"
-DIM="$(tput dim)"
-RST="$(tput sgr0)"
+BLD="\[$(tput bold)\]"
+DIM="\[$(tput dim)\]"
+RST="\[$(tput sgr0)\]"
 
 hostColour() {
   if is_ssh; then
