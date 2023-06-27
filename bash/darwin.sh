d32410206d06a952876c8d03131e8b18f959e7e0
Amber Cronin <amber@amber.vision>
2023-06-27 20:42:00 +0100

    Export EDITOR once

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -7,8 +7,6 @@
 alias hx='hx --config $HELIX_CONFIG'
 
 export VISUAL="hx --config $HELIX_CONFIG"
-export EDITOR="$VISUAL"
-export GIT_EDITOR="$VISUAL"
 
 export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/.brewfile"
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/ruby/bin:$PATH"
--- bash/linux.sh
+++ bash/linux.sh
@@ -8,5 +8,3 @@ alias open='xdg-open'
 alias hx='helix --config ${DOTFILES_DIR}/config/helix/config.toml'
 
 export VISUAL="helix --config $DOTFILES_DIR/config/helix/config.toml"
-export EDITOR="$VISUAL"
-export GIT_EDITOR="$VISUAL"
