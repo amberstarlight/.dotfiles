cfb25581e8d129501c4d744e4fadfb790c95022b
Amber Cronin <amber@amber.vision>
2023-07-09 21:41:37 +0100

    Specify XDG basedirs

--- bash/linux.sh
+++ bash/linux.sh
@@ -8,3 +8,9 @@ alias open='xdg-open'
 alias hx='helix --config ${DOTFILES_DIR}/config/helix/config.toml'
 
 export VISUAL="helix --config $DOTFILES_DIR/config/helix/config.toml"
+
+# set xdg base dirs on arch
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
