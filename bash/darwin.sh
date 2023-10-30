9a41523c761636d74b37436118fbd5c1592621cb
Amber Cronin <amber@amber.vision>
2023-10-30 15:43:32 +0000

    Add Ruby gems to path

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -11,6 +11,10 @@ export VISUAL="hx --config $HELIX_CONFIG"
 export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/.brewfile"
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/ruby/bin:$PATH"
 
+if which ruby >/dev/null && which gem >/dev/null; then
+  export PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+fi
+
 # todo: make PATH up from an array/list
 eval "$(rbenv init - bash)"
 
