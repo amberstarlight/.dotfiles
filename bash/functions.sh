e23b0477b9ef2fca86062d797cda41cf9475ed2a
Amber Cronin <amber@amber.vision>
2023-12-07 16:04:49 +0000

    Fix git branch prune function

--- bash/functions.sh
+++ bash/functions.sh
@@ -22,75 +22,19 @@ dot_clean () {
   env echo -n "Processed $deleted_count item$(optional_s "$deleted_count")."
 }
 
-git_all () {
-
-  BLUE='\033[1;34m'
-  RED='\033[1;30m'
-  NC='\033[0m'
-
-  for git_dir in */.git; do
-    (
-      repository=${git_dir%/*}
-
-      cd "$repository" || return;
-
-      # check if there's actually a remote
-      if git config --get remote.origin.url >/dev/null
-      then
-
-        # the first thing we should do is fetch from the remote
-        git fetch origin --prune --progress
-
-        # check if symbolic link to origin/HEAD exists
-        if ! git rev-parse --abbrev-ref origin/HEAD >/dev/null
-        then
-          # create the symbolic ref before we attempt to use it
-          git remote set-head origin -a;
-        fi
-
-        echo -e "${BLUE}Stashing existing changes...${NC}"
-        stash_result=$(git stash push -m "Local changes before pulling $(git_default_branch)")
-        needs_pop=1
-        if [ "$stash_result" = "No local changes to save" ]; then
-          needs_pop=0
-        fi
-
-        if [ ! "$(git_current_branch)" == "$(git_default_branch)" ]; then
-          git checkout "$(git_default_branch)";
-        fi
-
-        echo -e "${BLUE}Pulling updates from $(git_default_branch)...${NC}\n"
-        git pull origin "$(git_default_branch)"
-        echo
-
-        if [[ $needs_pop -eq 1 ]]; then
-          echo -e "${BLUE}Popping stashed changes...${NC}\n"
-          git stash pop
-        fi
-
-        unmerged_files=$(git diff --name-only --diff-filter=U)
-        if [[ -n $unmerged_files ]]; then
-          echo -e "${RED}The following files have merge conflicts after popping the stash:${NC}\n"
-          printf %"s\n" "$unmerged_files"  # Ensure newlines are printed
-          exit
-        else
-          git checkout "$(git_current_branch)"
-        fi
-
-      else
-        echo -e "${BLUE} ${repository} has no remote defined, skipping!${NC}\n"
-      fi
-
-    );
-  done
-}
-
 git_current_branch() {
   git rev-parse --abbrev-ref HEAD 2> /dev/null
 }
 
 git_default_branch() {
   git rev-parse --abbrev-ref origin/HEAD 2> /dev/null | cut -c8-
+}
+
+git_prune_branches() {
+  git branch --merged | grep -v main >/tmp/merged-branches && \
+  $EDITOR /tmp/merged-branches && \
+  xargs git branch -d </tmp/merged-branches \
+  && git fetch -pv
 }
 
 is_ssh() {
@@ -109,11 +53,4 @@ optional_s () {
   if [ ! "$1" -eq 1 ]; then
     env echo -n "s";
   fi
-}
-
-prune_git_branches() {
-  git branch --merged | grep -v main >/tmp/merged-branches && \
-  hx /tmp/merged-branches && \
-  xargs git branch -d </tmp/merged-branches \
-  && git fetch -pv
 }
