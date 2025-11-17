7263e279dd7394a1933746765a9ec53860c17ad1
Amber Cronin <amber@amber.vision>
2025-11-17 15:14:06 +0000

    Forcibly take control of system ruby on darwin

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -9,11 +9,10 @@ export VISUAL="hx"
 export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/.brewfile"
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/ruby/bin:$PATH"
 
-if which ruby >/dev/null && which gem >/dev/null; then
-  export PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
-fi
-
-# todo: make PATH up from an array/list
-eval "$(rbenv init - bash)"
-
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+
+# override darwin system ruby, forcibly
+if [ -d "/usr/local/opt/ruby/bin" ]; then
+  export PATH=/usr/local/opt/ruby/bin:$PATH
+  export PATH=$(gem environment gemdir)/bin:$PATH
+fi
