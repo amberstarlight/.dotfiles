a2252c60d7beebf82121ea1e56f93904f95dc192
Amber Cronin <amber@amber.vision>
2025-11-18 15:46:25 +0000

    Stop postgres attempting to use GSS encryption on darwin

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -16,3 +16,6 @@ if [ -d "/usr/local/opt/ruby/bin" ]; then
   export PATH=/usr/local/opt/ruby/bin:$PATH
   export PATH=$(gem environment gemdir)/bin:$PATH
 fi
+
+# force libpq to not use GSS encryption
+export PGGSSENCMODE="disable"
